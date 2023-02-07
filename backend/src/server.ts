7c28112aad7c34b8ecce0101792f4cc10ef00c97
MingZhe <trendy0413dev@gmail.com>
2023-02-07 23:16:13 +0200

    established mongo connection

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,14 +1,17 @@
 import express from 'express'
+import mongoose from 'mongoose'
 
 import compression from 'compression'
 import cors from 'cors'
 
+import { MONGODB_URI } from './util/secrets'
 class Server {
   public app: express.Application
 
   constructor() {
     this.app = express()
     this.config()
+    this.mongo()
   }
 
   public config(): void {
@@ -19,6 +22,45 @@ class Server {
     this.app.use(cors())
   }
 
+  private mongo() {
+    const connection = mongoose.connection
+    connection.on('connected', () => {
+      console.log('Mongo Connection Established')
+    })
+    connection.on('reconnected', () => {
+      console.log('Mongo Connection Reestablished')
+    })
+    connection.on('disconnected', () => {
+      console.log('Mongo Connection Disconnected')
+      console.log('Trying to reconnect to Mongo ...')
+      setTimeout(() => {
+        mongoose.connect(MONGODB_URI, {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+          autoReconnect: true,
+          keepAlive: true,
+          socketTimeoutMS: 3000,
+          connectTimeoutMS: 3000,
+        })
+      }, 3000)
+    })
+    connection.on('close', () => {
+      console.log('Mongo Connection Closed')
+    })
+    connection.on('error', (error: Error) => {
+      console.log('Mongo Connection ERROR: ' + error)
+    })
+
+    const run = async () => {
+      await mongoose.connect(MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        keepAlive: true,
+      })
+    }
+    run().catch((error) => console.error(error))
+  }
+  
   public start(): void {
     this.app.listen(this.app.get('port'), () => {
       console.log('Lithium auth backend is running at http://localhost:%d', this.app.get('port'))
---
+++ backend/src/util/secrets.ts
@@ -0,0 +1,2 @@
+export const MONGODB_URI = "mongodb://localhost:27017/lithium"
+export const JWT_SECRET = "lithium_secret_key"
